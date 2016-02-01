d421e3ff423588c49097a08cdb959257e3d44d42
Klaus Nyrnberg <klaus@nyrnberg.dk>
2016-02-01 13:22:48 +0100

    Initial check-in of module TestProjekt

---
+++ TestProjekt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+namespace TestProjekt
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Console.WriteLine ("Hello world");
+
+			Application.Run ();
+		}
+	}
+}
