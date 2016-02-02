2f50fd56eb56fbbaac3f3d7832888c6c48be8e73
klaus <klaus@nyrnberg.dk>
2016-02-02 09:49:20 +0100

    asdf

---
+++ TestProjekt/Dialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestProjekt
+{
+	public partial class Dialog : Gtk.Dialog
+	{
+		public Dialog ()
+		{
+			this.Build ();
+		}
+	}
+}
+
--- TestProjekt/MainWindow.cs
+++ TestProjekt/MainWindow.cs
@@ -3,14 +3,22 @@ using Gtk;
 
 public partial class MainWindow: Gtk.Window
 {
+	Button gtkClickMe;
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
+		gtkClickMe = new Button ();
+		gtkClickMe.Label = "str";
+		this.Add (gtkClickMe);
+
+		foreach (var w in this.Children) 
+		{
+			Console.WriteLine (w.Name);
+		}
 		Build ();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
-		a.RetVal = true;
 	}
 }
